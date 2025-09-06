7ef37491fa9ca7ebd573c601470fd1bed7fc5b66
nikhilhmane49 <manenikhil1949@gmail.com>
2025-09-06 17:39:10 +0530

    fun type I

---
+++ c/Assignment/Assignment_06_01_Type_1_function.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+
+
+//**********Write a program to check whether a number is even or odd. */
+// void checkEvenOdd();
+// void main()
+// {
+//     checkEvenOdd();
+// }
+
+// void checkEvenOdd(){
+//     int num;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+//     if(num%2==0){
+//         printf("The number %d is even.\n", num);
+//     }
+//     else{
+//         printf("The number %d is odd.\n", num);
+//     }
+// }
+
+//************* Write a program to check given 3 digit number is pallindrome or not.*/
+
+// void checkPallindrome();
+// void main()
+// {
+//     checkPallindrome();
+// }
+
+// void checkPallindrome(){
+//     printf("Enter a 3 digit number: ");
+//     int num, originalNum, reversedNum = 0, digit;
+//     scanf("%d", &num);
+//     originalNum = num;
+
+//     while(num !=0){
+//         digit = num % 10;
+//         reversedNum = reversedNum * 10 + digit;
+//         num = num / 10;
+//     }
+//     if(originalNum == reversedNum){
+//         printf("The number %d is pallindrome.\n", originalNum);
+//     }
+//     else{
+//         printf("The number %d is not pallindrome.\n", originalNum);
+//     }
+// }
+
+
+
+
+//************* */
