ef6cac62d1094ed5cc3d03167f4b1dd6cb5da1bc
nikhilhmane49 <manenikhil1949@gmail.com>
2025-09-18 19:20:02 +0530

    pointer and array

--- c/Assignment/Assignment_07.c
+++ c/Assignment/Assignment_07.c
@@ -1,25 +1,26 @@
 //^^^^^^^Pointer
 
 #include <stdio.h>
+#include <stdbool.h>
 
 //**********swap two numbers */
 
 // void swap(int*, int*);
-// void main(){
-//     int a = 10;
-//     int b = 20;
+void main(){
+    int a = 10;
+    int b = 20;
 
-//     printf("Before swap: a = %d, b = %d\n", a, b);
-//     swap(&a, &b);
-//     printf("After swap: a = %d, b = %d\n", a, b);
-// }
+    printf("Before swap: a = %d, b = %d\n", a, b);
+    swap(&a, &b);
+    printf("After swap: a = %d, b = %d\n", a, b);
+}
 
-// void swap(int *x,int *y){
-//     int temp;
-//     temp = *x;
-//     *x = *y;
-//     *y = temp;
-// }
+void swap(int *x,int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
 
 //**********Write a program to input an array using pointers and print its elements. */
 
@@ -63,4 +64,94 @@ int main(){
             min = *(ptr + i);
         }
     }
+}
+
+//**********Write a program to input an array using pointers and print its elements. */
+
+void StoreArray(int *ptr, int size);
+
+void DisplayArray(int *ptr, int size);
+
+int main()
+{
+    int arr[5];
+    int brr[6];
+    int crr[8];
+
+    printf("Enter 5 values in the first array:\n");
+    StoreArray(arr, 5);
+    printf("Enter 6 values in the second array:\n");
+    StoreArray(brr, 6);
+    printf("Enter 7 values in the third array:\n");
+    StoreArray(crr, 7);
+
+    printf("The elements in first array are:\n");
+    DisplayArray(arr, 5);
+    printf("\nThe elements in second array are:\n");
+    DisplayArray(brr, 6);
+    printf("\nThe elements in third array are:\n");
+    DisplayArray(crr, 7);
+}
+
+void StoreArray(int *ptr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+}
+
+void DisplayArray(int *ptr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", ptr[i]);
+    }
+}
+
+//**********Write a program to search the element in the array using pointer  */
+
+void StoreArray(int *ptr, int size);
+
+void SearchElement(int *ptr, int size, int num);
+
+int main()
+{
+    int arr[5];
+
+    printf("Enter 5 values in the first array:\n");
+    StoreArray(arr, 5);
+
+    int num;
+    printf("Enter the number to search: ");
+    scanf("%d", &num);
+
+    SearchElement(arr, 5, num);
+}
+
+void StoreArray(int *ptr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &ptr[i]);
+    }
+}
+
+void SearchElement(int *ptr, int size, int num)
+{
+    bool found = false;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (num == ptr[i])
+        {
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+        printf("Number %d found in the array.\n", num);
+    else
+        printf("Number %d not found in the array.\n", num);
 }
--- c/Class_work/hello.c
+++ c/Class_work/hello.c
@@ -95,28 +95,159 @@
 // }
 
 
-int main(){
-    int arr[10];
-    printf("Enter 10 values in the array:\n");
-    for (int i = 0; i < 10; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
+// int main(){
+//     int arr[10];
+//     printf("Enter 10 values in the array:\n");
+//     for (int i = 0; i < 10; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
 
-    for (int i = 0; i < 10;i++){
-        int isduplicate = 0;
-        for (int j = i + 1; j < 10;j++){
-            if(arr[i]==arr[j]){
-                isduplicate = 1;
-                break;
-            }
-        }
+//     for (int i = 0; i < 10;i++){
+//         int isduplicate = 0;
+//         for (int j = i + 1; j < 10;j++){
+//             if(arr[i]==arr[j]){
+//                 isduplicate = 1;
+//                 break;
+//             }
+//         }
 
-        if (isduplicate == 0)
-        {
-            printf("%d ", arr[i]);
-        }
-    }
+//         if (isduplicate == 0)
+//         {
+//             printf("%d ", arr[i]);
+//         }
+//     }
 
   
+// }
+
+
+
+//***********take the three number form user and store it and display it */
+
+// int main(){
+//     int arr[4];
+//     int brr[6];
+//     int crr[8];
+
+//     printf("Enter 4 values in the first array:\n");
+//     for (int i = 0; i < 4; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     printf("Enter 6 values in the second array:\n");
+//     for (int i = 0; i < 6; i++)
+//     {
+//         scanf("%d", &brr[i]);
+//     }
+//     printf("Enter 8 values in the third array:\n");
+//     for (int i = 0; i < 8; i++)
+//     {
+//         scanf("%d", &crr[i]);
+//     }
+
+//     printf("The elements in first array are:\n");
+//     for (int i = 0; i < 4; i++)
+//     {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\nThe elements in second array are:\n");
+//     for (int i = 0; i < 6; i++)
+//     {
+//         printf("%d ", brr[i]);
+//     }
+//     printf("\nThe elements in third array are:\n");
+//     for (int i = 0; i < 8; i++)
+//     {
+//         printf("%d ", crr[i]);
+//     }
+// }
+
+
+
+// //**********Write a program to input an array using pointers and print its elements. */
+
+// void StoreArray(int *ptr, int size);
+
+// void DisplayArray(int *ptr, int size);
+
+// int main(){
+//     int arr[5];
+//     int brr[6];
+//     int crr[8];
+
+//     printf("Enter 5 values in the first array:\n");
+//     StoreArray(arr, 5);
+//     printf("Enter 6 values in the second array:\n");
+//     StoreArray(brr, 6);
+//     printf("Enter 7 values in the third array:\n");
+//     StoreArray(crr, 7);
+
+
+
+//     printf("The elements in first array are:\n");
+//     DisplayArray(arr, 5);
+//     printf("\nThe elements in second array are:\n");
+//     DisplayArray(brr, 6);
+//     printf("\nThe elements in third array are:\n");
+//     DisplayArray(crr, 7);
+
+// }
+
+// void StoreArray(int *ptr, int size){
+//     for (int i = 0; i < size;i++){
+//         scanf("%d", &ptr[i]);
+//     }
+// }
+
+// void DisplayArray(int *ptr, int size){
+//     for (int i = 0; i < size;i++){
+//         printf("%d ", ptr[i]);
+//     }
+// }
+
+
+
+//**********Write a program to search the element in the array using pointer  */
+
+void StoreArray(int *ptr, int size);
+
+void SearchElement(int *ptr, int size, int num);
+
+int main(){
+    int arr[5];
+
+
+    printf("Enter 5 values in the first array:\n");
+    StoreArray(arr, 5);
+
+    int num;
+    printf("Enter the number to search: ");
+    scanf("%d", &num);
+
+    SearchElement(arr, 5, num);
+}
+
+
+void StoreArray(int *ptr, int size){
+    for (int i = 0; i < size;i++){
+        scanf("%d", &ptr[i]);
+    }
+}
+
+void SearchElement(int *ptr, int size, int num)
+{
+    bool found = false;
+
+    for (int i = 0; i < size;i++){
+        if(num==ptr[i]){
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+        printf("Number %d found in the array.\n", num);
+    else
+        printf("Number %d not found in the array.\n", num);
 }
