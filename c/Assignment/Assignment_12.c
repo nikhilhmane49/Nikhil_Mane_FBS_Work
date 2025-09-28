574cf923642420e0ed490196187fe4275f1209ad
nikhilhmane49 <manenikhil1949@gmail.com>
2025-09-28 16:12:50 +0530

    string ass

--- c/Assignment/Assignment_12.c
+++ c/Assignment/Assignment_12.c
@@ -3,240 +3,271 @@
 
 //***********Write a program to scan string from user then scan a single character and search it
 // in a accepted string.* /
-//     int main()
-// {
-//     char str[10];
-//     int flag = 0;
+    int main()
+{
+    char str[10];
+    int flag = 0;
 
-//     printf("Enter the string");
-//     scanf("%s", str);
+    printf("Enter the string");
+    scanf("%s", str);
 
-//     fflush(stdin);
+    fflush(stdin);
 
-//     char ch;
-//     printf("Enter the char to find: ");
-//     scanf("%c", &ch);
+    char ch;
+    printf("Enter the char to find: ");
+    scanf("%c", &ch);
 
-//     int i = 0;
-//     while(str[i]!='\0'){
-//         if(str[i]==ch){
-//             flag = 1;
-//             break;
-//         }
-//         i++;
-//     }
+    int i = 0;
+    while(str[i]!='\0'){
+        if(str[i]==ch){
+            flag = 1;
+            break;
+        }
+        i++;
+    }
 
-//     if(flag==0){
-//         printf("Char is not found");
-//     }
-//     else{
-//         printf("Char is found");
-//     }
-// }
+    if(flag==0){
+        printf("Char is not found");
+    }
+    else{
+        printf("Char is found");
+    }
+}
 
 //*****WAP Replace all Occurrences of ‘a’ with $ in a String */
-// int main(){
-//     char str[20];
-//     printf("Enter the string : ");
-//     scanf("%s", str);
+int main(){
+    char str[20];
+    printf("Enter the string : ");
+    scanf("%s", str);
 
-//     int i = 0;
-//     while(str[i]!='\0'){
-//         if(str[i]=='a'){
-//             str[i] = '$';
-//         }
-//         i++;
-//     }
+    int i = 0;
+    while(str[i]!='\0'){
+        if(str[i]=='a'){
+            str[i] = '$';
+        }
+        i++;
+    }
 
-//     printf("%s", str);
-// }
+    printf("%s", str);
+}
 
 //********WAP to Remove the nth Index Character from a Non-Empty String */
 
-// int main(){
-//     char str[20];
+int main(){
+    char str[20];
 
-//     printf("Enter the string: ");
+    printf("Enter the string: ");
 
-//     scanf("%s", str);
+    scanf("%s", str);
 
-//     char len=strlen(str);
+    char len=strlen(str);
 
-//     printf("The length of string is %d\n", len);
-//     printf("The string is %s\n", str);
+    printf("The length of string is %d\n", len);
+    printf("The string is %s\n", str);
 
-//     int idx;
-//     printf("Enter the index to remove: ");
-//     scanf("%d", &idx);
+    int idx;
+    printf("Enter the index to remove: ");
+    scanf("%d", &idx);
 
-//     for (int i = idx; str[i] != '\0';i++){
-//         str[i] = str[i + 1];
-//         printf("%s\n", str);
-//     }
+    for (int i = idx; str[i] != '\0';i++){
+        str[i] = str[i + 1];
+        printf("%s\n", str);
+    }
 
 
-//     printf("String after removing the char at index %d is %s", idx, str);
-// }
+    printf("String after removing the char at index %d is %s", idx, str);
+}
 
 //********WAP to Form a New String where the First Character and the Last Character have
 // been Exchanged * /
 
-// int main()
-// {
+int main()
+{
 
-//     char str[20];
+    char str[20];
 
-//     printf("Enter the string: ");
-//     scanf("%s", str);
-//     int len = strlen(str);
-//     printf("The length of string is %d\n", len);
-//         char temp = str[0];
-//         str[0] = str[len - 1];
-//         str[len - 1] = temp;
+    printf("Enter the string: ");
+    scanf("%s", str);
+    int len = strlen(str);
+    printf("The length of string is %d\n", len);
+        char temp = str[0];
+        str[0] = str[len - 1];
+        str[len - 1] = temp;
 
-//     printf("The modified string is %s", str);
-// }
+    printf("The modified string is %s", str);
+}
 
 //******WAP to Count the Number of Vowels in a String */
 
-// int main(){
-//     char str[20];
-//     printf("Enter the string: ");
-//     scanf("%s", str);
+int main(){
+    char str[20];
+    printf("Enter the string: ");
+    scanf("%s", str);
 
-//     int i = 0;
-//     int count = 0;
-//     while(str[i]!='\0'){
-//         if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
-//            str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'){
-//             count++;
-//         }
-//         i++;
-//     }
+    int i = 0;
+    int count = 0;
+    while(str[i]!='\0'){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U'){
+            count++;
+        }
+        i++;
+    }
 
-//     printf("The number of vowels in the string is %d", count);
-// }
+    printf("The number of vowels in the string is %d", count);
+}
 
+//****** WAP to Take in a String and Replace Every Blank Space with special symbol.*/
+int main(){
+    char str[20];
+    printf("Enter the string: ");
+    // scanf("%s", str);
 
+    gets(str); 
 
-// int main(){
-//     char str[20];
-//     printf("Enter the string: ");
-//     scanf("%s", str);
+    printf("The original string is %s\n", str);
 
-//     int i = 0;
-//     while(str[i]!='\0'){
-//         if(str[i]=='&'){
-//             str[i] = '$';
-//         }
-//         i++;
-//     }
+    int i = 0;
+    while(str[i]!='\0'){
+        if(str[i]==' '){
+            str[i] = '$';
+        }
+        i++;
+    }
 
-//     printf("The modified string is %s", str);
-// }
+    printf("The modified string is %s", str);
+}
 
-//**** */
+//****WAP to Remove the Characters of Odd Index Values in a String */
 
-// int main(){
-//     char str[20];
-//     printf("Enter the string: ");
-//     scanf("%s", str);
+int main(){
+    char str[20];
+    char res[20];
+    printf("Enter the string: ");
+    scanf("%s", str);
 
-//     int i = 0;
-//     while(str[i]!='\0'){
-//         if(i%2!=0){
-//             str[i] = st;
-//         }
-//         i++;
-//     }
+    int i = 0;
+    int j = 0;
+    while(str[i]!='\0'){
+        if(i%2==0){
+            res[j] = str[i];
+            j++;
+        }
+        
+        i++;
+    }
 
-//     printf("The modified string is %s", str);
-// }
+    res[j] = '\0';
+
+    printf("The modified string is %s", res);
+}
 
 //********WAP to Take in Two Strings and Display the Larger String without Using Built-in
 // Functions * /
 
-//     int main(){
-//     char str1[20];
-//     char str2[20];
+    int main(){
+    char str1[20];
+    char str2[20];
 
-//     printf("Enter the first string: ");
-//     scanf("%s", str1);
-//     printf("Enter the second string: ");
-//     scanf("%s", str2);
+    printf("Enter the first string: ");
+    scanf("%s", str1);
+    printf("Enter the second string: ");
+    scanf("%s", str2);
 
-//     int len1 = 0;
-//     int len2 = 0;
+    int len1 = 0;
+    int len2 = 0;
 
-//     while(str1[len1]!='\0'){
-//         len1++;
-//     }
+    while(str1[len1]!='\0'){
+        len1++;
+    }
 
-//     while(str2[len2]!='\0'){
-//         len2++;
-//     }
+    while(str2[len2]!='\0'){
+        len2++;
+    }
 
 
-//     if(len1<len2){
-//         printf("The secoud  string is greater");
-//     }
-//     else if(len1>len2){
-//         printf("The frirst string is smaller");
-//     }
-//     else{
-//         printf("Both strings are equal");
-//     }
-// }
+    if(len1<len2){
+        printf("The secoud  string is greater");
+    }
+    else if(len1>len2){
+        printf("The frirst string is smaller");
+    }
+    else{
+        printf("Both strings are equal");
+    }
+}
 
 //*********Write a program to check the string is palindrome or not.(with using in bulid fun) */
 
 
 
-    // int main(){
-    //     char str[20];
-    //     printf("Enter the string: ");
-    //     scanf("%s", str);
-    //     char rev[20];
+    int main(){
+        char str[20];
+        printf("Enter the string: ");
+        scanf("%s", str);
+        char rev[20];
 
-    //     strcpy(rev, str);
+        strcpy(rev, str);
 
-    //     int res = strcmp(str, strrev(rev));
+        int res = strcmp(str, strrev(rev));
 
-    //     if(res==0){
-    //         printf("The string is palindrome");
-    //     }
-    //     else{
-    //         printf("The string is not palindrome");
-    //     }
+        if(res==0){
+            printf("The string is palindrome");
+        }
+        else{
+            printf("The string is not palindrome");
+        }
 
-    //     printf("The result is %d", res);
-    // }
+        printf("The result is %d", res);
+    }
 
     //*********Write a program to check the string is palindrome or not.(without using in bulid fun) */
-//     int main()
-//     {
-//         char str[20];
-//         printf("enter the string:");
-//         scanf("%s", str);
-//         int len = strlen(str);
-//         printf("The length of the string is %d", len);
+    int main()
+    {
+        char str[20];
+        printf("enter the string:");
+        scanf("%s", str);
+        int len = strlen(str);
+        printf("The length of the string is %d", len);
 
-//         int flag = 0;
+        int flag = 0;
 
-//         for (int i = 0; i < len / 2; i++)
-//         {
-//             if (str[i] != str[len - i - 1])
-//             {
-//                 flag = 1;
-//                 break;
-//             }
-//         }
+        for (int i = 0; i < len / 2; i++)
+        {
+            if (str[i] != str[len - i - 1])
+            {
+                flag = 1;
+                break;
+            }
+        }
 
-//         if (flag == 0)
-//         {
-//             printf("The string is palindrome");
-//         }
-//         else{
-//         printf("The string is not palindrome");
-//     }
-// }
+        if (flag == 0)
+        {
+            printf("The string is palindrome");
+        }
+        else{
+        printf("The string is not palindrome");
+    }
+}
+
+//*********WAP to Calculate the Number of Words Present in a String */
+
+int main(){
+    char str[20];
+    printf("Enter the string:");
+    gets(str);
+    printf("the str is%s \n :", str);
+
+    int count = 1;
+    int i = 0;
+    while(str[i]!='\0'){
+        if(str[i]==' '){
+            count++;
+        }
+        i++;
+    }
+   
+
+    printf("the total number of words is %d", count);
+}
+
